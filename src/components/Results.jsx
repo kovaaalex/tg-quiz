e96235a99f48508cf615360780de0883653ca953
kovaaalex <kovalenko.alex04@gmail.com>
2024-11-09 04:50:30 +0300

    add tgg

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -3,37 +3,31 @@ import axios from "axios";
 
 export default function Results({ score }) {
     const totalQuestions = 3;
+    const token = '7470728509:AAESPkm1XRq4rynh1rOndGmS6kF5twHHAHY'; // Ваш токен
+    const chatId = '856499347'; // Ваш chatId
 
     useEffect(() => {
-        const tg = window.Telegram ? window.Telegram.WebApp : null;
+        const sendTelegramMessage = async () => {
+            const message = `Я прошёл квиз и набрал ${score} из ${totalQuestions} баллов!`;
+            console.log('Отправка сообщения:', message); // Отладочное сообщение
+            try {
+                const response = await axios.post(`https://api.telegram.org/bot${token}/sendMessage`, {
+                    chat_id: chatId,
+                    text: message
+                });
+                console.log('Message sent:', response.data);
+            } catch (error) {
+                console.error('Error sending message:', error.response ? error.response.data : error.message);
+            }
+        };
 
-        if (tg) {
-            tg.MainButton.setText("Отправить результат");
-            tg.MainButton.show();
-
-            tg.MainButton.onClick(async () => {
-                const token = '7470728509:AAESPkm1XRq4rynh1rOndGmS6kF5twHHAHY'; // Ваш токен
-                const chatId = '856499347'; // Ваш chatId
-                const message = `Я прошёл квиз и набрал ${score} из ${totalQuestions} баллов!`;
-
-                try {
-                    const response = await axios.post(`https://api.telegram.org/bot${token}/sendMessage`, {
-                        chat_id: chatId,
-                        text: message
-                    });
-                    console.log('Message sent:', response.data);
-                } catch (error) {
-                    console.error('Error sending message:', error.response ? error.response.data : error.message);
-                }
-
-                tg.MainButton.hide(); // Скрываем кнопку после отправки
-            });
-        } else {
-            console.log("Запустите приложение внутри Telegram для доступа к Web App API.");
+        // Отправка сообщения только если score изменился
+        if (score !== null) {
+            sendTelegramMessage();
         }
     }, [score]);
 
     return (
-        <h2>Results: {score} from, {totalQuestions}</h2>
+        <h2>Results: {score} from {totalQuestions}</h2>
     );
 }
