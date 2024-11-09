e42a508c6e516306ab319da9ce8f4dc4e6825811
kovaaalex <kovalenko.alex04@gmail.com>
2024-11-09 03:30:36 +0300

    add tg

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,9 +1,18 @@
 import React, {useEffect} from "react"
 export default function Results({score}) {
+    const totalQuestions = 3
+    useEffect(() => {
+        const tg = window.Telegram ? window.Telegram.WebApp : null;
+
+if (tg) {
+  // Используем методы tg, если объект доступен
+  tg.MainButton.setText("Отправить результат");
+  tg.MainButton.show();
+} else {
+  console.log("Запустите приложение внутри Telegram для доступа к Web App API.");
+}
+    }, [score])
     return (
-      <>
-        <h2>Results</h2>
-        <h3>{score}</h3>
-        </>
+        <h2>Results {score} from {totalQuestions}</h2>
     )
 }
